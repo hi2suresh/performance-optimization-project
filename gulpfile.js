72339df6bbe24cf0f0c2c295ea98f0a78764b010
hi2suresh <hi2suresh@gmail.com>
2017-03-17 17:55:31 +1000

    Updated gulp.js file to concat and minify css and javascripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    uglifycss = require('gulp-uglifycss');
 
 gulp.task('concatCSS', function() {
-    gulp.src(['css/normalize.css','css/foundation.css','css/arvo.css', 'css/ubuntu.css',
+    return gulp.src(['css/normalize.css','css/foundation.css','css/arvo.css', 'css/ubuntu.css',
               'css/basics.css','css/menu.css','css/hero.css','css/photo-grid.css',
               'css/modals.css','css/footer.css'])
         .pipe(concat('app.css'))
@@ -12,13 +15,28 @@ gulp.task('concatCSS', function() {
     
 });
 
+gulp.task('minifyCSS', ['concatCSS'], function(){
+    gulp.src('css/app.css')
+        .pipe(uglifycss())
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('css'));
+});
+
+
 gulp.task('concatScripts', function() {
-    gulp.src(['js/jquery.js','js/fastclick.js','js/foundation.js','js/foundation.equalizer.js',
+    return gulp.src(['js/jquery.js','js/fastclick.js','js/foundation.js','js/foundation.equalizer.js',
               'js/foundation.reveal.js'])
         .pipe(concat('app.js'))
         .pipe(gulp.dest('js'));
+}); 
+
+gulp.task('minifyScripts', ['concatScripts'], function(){
+   gulp.src('js/app.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js'));
 });
 
-gulp.task('default',['concatCSS','concatScripts'], function() {
+gulp.task('default',['minifyCSS','minifyScripts'], function() {
     console.log('Default Task');
 })
